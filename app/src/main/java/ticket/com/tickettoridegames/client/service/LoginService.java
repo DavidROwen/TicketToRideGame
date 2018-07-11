dc5dad21c680bb8583ced35d35e72d3370b7ba52
fr1gid <d4v1d.0w3n@gmail.com>
2018-07-11 12:40:34 -0600

    Add view stubs.

--- app/src/main/java/ticket/com/tickettoridegames/client/service/LoginService.java
+++ app/src/main/java/ticket/com/tickettoridegames/client/service/LoginService.java
@@ -1,10 +1,13 @@
 package ticket.com.tickettoridegames.client.service;
 
 import ticket.com.tickettoridegames.utility.model.User;
+import ticket.com.tickettoridegames.utility.web.Command;
 
 public class LoginService {
 
-    void login(User user){}
+    void login(User user){
+//        Command loginCommand = new Command(ticket.com.tickettoridegames.server.service.LoginService.class.getName(), ticket.com.tickettoridegames.server.service.LoginService.class,)
+    }
 
     void register(User user){}
 
