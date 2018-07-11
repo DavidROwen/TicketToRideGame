d8b86bfbfe6e56e8f1a7c600c6bbe92fddcfe823
Dillon Astin <astindillon@gmail.com>
2018-07-11 16:03:39 -0600

    Fleshed out CommandManager to store queues based on playerID

--- app/src/main/java/ticket/com/tickettoridegames/server/CommandsManager.java
+++ app/src/main/java/ticket/com/tickettoridegames/server/CommandsManager.java
@@ -1,5 +1,7 @@
 package ticket.com.tickettoridegames.server;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 import ticket.com.tickettoridegames.utility.web.Command;
@@ -13,31 +15,65 @@ public class CommandsManager {
     }
 
     private CommandsManager() {
-        this.commands = new PriorityQueue<>();
+        this.commands = new HashMap<>();
     }
 
-    private PriorityQueue<Command> commands;
+    private Map<String, PriorityQueue<Command>> commands;
 
-    public static PriorityQueue<Command> getCommands() { //todo divide them by person maybe
-        PriorityQueue<Command> passedCommands = new PriorityQueue<>();
-        copyCommandsOver(passedCommands);
-
-        return passedCommands;
+    private static boolean isEmpty(String playerId){
+        PriorityQueue<Command> queue = instance().commands.get(playerId);
+        if(queue == null){
+            //queue does not exist yet therefore is empty
+            return true;
+        }
+        else{
+            if(queue.isEmpty()){
+                //queue is empty
+                return true;
+            }
+            //queue is not empty
+            return false;
+        }
     }
 
-    private static void copyCommandsOver(PriorityQueue<Command> passedCommands) {
+    //gets the queue of commands for a given user
+    public static PriorityQueue<Command> getCommands(String playerId) {
+        if(isEmpty(playerId)) {
+            //no commands to be sent, return null
+            return null;
+        }
+        else{
+            PriorityQueue<Command> passedCommands = new PriorityQueue<>();
+            copyCommandsOver(passedCommands, playerId);
+            clear(playerId);
+            return passedCommands;
+        }
+    }
+
+    //copies commands over to be sent. Makes it easier to clear a queue.
+    private static void copyCommandsOver(PriorityQueue<Command> passedCommands, String playerId) {
         Command next;
         do {
-            next = instance().commands.poll();
+            next = instance().commands.get(playerId).poll();
             if(next != null) { passedCommands.add(next); }
         }while(next != null);
     }
 
+    //adds a command to a users queue. If the user does not have a queue yet it creates one
     public static void addCommand(Command command, String playerId) {
-        instance().commands.add(command);
+        PriorityQueue<Command> pq = instance().commands.get(playerId);
+        if(pq == null){
+            pq = new PriorityQueue<>();
+            pq.add(command);
+            instance().commands.put(playerId, pq);
+        }
+        else{
+            pq.add(command);
+        }
     }
 
-    public static void clear() {
-        instance().commands.clear();
+    //clear a users queue. used after sending a queue of commands.
+    private static void clear(String playerId) {
+        instance().commands.get(playerId).clear();
     }
 }
