97fcee1a50e5a1fc00335f0a1969db8cabb4e382
Ezra Clark <ezrajclark@gmail.com>
2018-07-12 13:30:33 -0600

    updated views

---
+++ app/src/main/java/ticket/com/tickettoridegames/client/view/GameActivity.java
@@ -0,0 +1,15 @@
+package ticket.com.tickettoridegames.client.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ticket.com.tickettoridegames.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
