7fb109c38b2d2e2813e59197579a27ec0ea1310b
fr1gid <d4v1d.0w3n@gmail.com>
2018-07-12 13:20:04 -0600

    Work on the login activity to properly get text fields. Fix android manifest to allow internet access.

--- app/src/main/java/ticket/com/tickettoridegames/server/service/LoginService.java
+++ app/src/main/java/ticket/com/tickettoridegames/server/service/LoginService.java
@@ -8,6 +8,8 @@ public class LoginService {
 
     public Result login(String username, String password){
 
+        System.out.println("Login request received: Username:"+username+" Password:"+password);
+
         Result result = new Result();
         ServerModel sm = ServerModel.getInstance();
 
