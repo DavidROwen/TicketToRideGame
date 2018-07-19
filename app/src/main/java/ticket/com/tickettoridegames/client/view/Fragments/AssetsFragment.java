67e663e8048c652688757fdf8a0fdf18eb99b817
Ezra Clark <ezrajclark@gmail.com>
2018-07-19 17:57:59 -0600

    Merge branch 'staticServices' of https://github.com/DavidROwen/TicketToRideGame into EzBranch
    # Conflicts:
    #	app/src/main/java/ticket/com/tickettoridegames/client/view/JoinActivity.java
    #	app/src/main/res/layout/customlayout.xml

---
+++ app/src/main/java/ticket/com/tickettoridegames/client/view/Fragments/AssetsFragment.java
@@ -0,0 +1,38 @@
+package ticket.com.tickettoridegames.client.view.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import ticket.com.tickettoridegames.R;
+
+public class AssetsFragment extends BasicFragment{
+    @Override
+    public BasicFragment provideYourFragment() {
+
+        return new StatsFragment();
+    }
+
+    @Override
+    public View provideYourFragmentView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_assets,parent,false);
+
+        //Get your parent layout of fragment
+        RelativeLayout layout = (RelativeLayout)view;
+
+        //Now specific components here
+
+        Button b1 = (Button)layout.findViewById(R.id.button1);
+        Button b2 = (Button)layout.findViewById(R.id.button2);
+
+
+        return view;
+
+    }
+
+
+}
