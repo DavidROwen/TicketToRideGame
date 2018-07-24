62f78c057ef1ba17746530f011090318d7a9b4ef
Ezra Clark <ezrajclark@gmail.com>
2018-07-24 08:57:32 -0600

    fragments

--- app/src/main/java/ticket/com/tickettoridegames/client/view/IMapView.java
+++ app/src/main/java/ticket/com/tickettoridegames/client/view/IMapView.java
@@ -1,5 +1,9 @@
 package ticket.com.tickettoridegames.client.view;
 
+import java.util.Set;
+
+import ticket.com.tickettoridegames.utility.model.DestinationCard;
+
 public interface IMapView {
 
     void displayMessage(String message);
@@ -7,4 +11,8 @@ public interface IMapView {
     void enableTurn();
 
     void disableTurn();
+
+    void displayDestinationCards(Set<DestinationCard> destinationCards);
+
+    void getDestinationCardChoices();
 }
