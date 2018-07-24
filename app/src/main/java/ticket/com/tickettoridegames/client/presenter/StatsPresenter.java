3ad79bbab8f69392f9b27abd37bd4da95336a8c7
fr1gid <d4v1d.0w3n@gmail.com>
2018-07-23 20:22:56 -0600

    Fix issue with null chat messages breaking game.

--- app/src/main/java/ticket/com/tickettoridegames/client/presenter/StatsPresenter.java
+++ app/src/main/java/ticket/com/tickettoridegames/client/presenter/StatsPresenter.java
@@ -58,10 +58,13 @@ public class StatsPresenter implements IStatsPresenter , Observer {
         String userID = clientModel.getUserId();
         String gameID = clientModel.getUser().getGameId();
         if (gameID == null || gameID.equals("")){
-            statsView.displayMessage("Invalid game ID");
+            statsView.displayMessage("Invalid game ID, failed to send.");
         }
         else if (userID == null || userID.equals("")){
-            statsView.displayMessage("Invalid player ID");
+            statsView.displayMessage("Invalid player ID, failed to send.");
+        }
+        else if (message == null || message.isEmpty()){
+            statsView.displayMessage("Invalid message, failed to send.");
         }
         else {
             Result result = LobbyService.sendChat(gameID,userID,message);
