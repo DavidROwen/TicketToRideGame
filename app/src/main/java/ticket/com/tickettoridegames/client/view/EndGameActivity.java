76a6e551e8aeab47fe77ab9e9854422058ee150f
Ezra Clark <ezrajclark@gmail.com>
2018-07-26 11:31:30 -0600

    fragments

---
+++ app/src/main/java/ticket/com/tickettoridegames/client/view/EndGameActivity.java
@@ -0,0 +1,15 @@
+package ticket.com.tickettoridegames.client.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ticket.com.tickettoridegames.R;
+
+public class EndGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+    }
+}
