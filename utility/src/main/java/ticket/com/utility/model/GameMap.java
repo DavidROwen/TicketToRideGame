0b0982d7e9220b350829a17c8134972c29519ee6
aaronm-12 <aarondmc12@gmail.com>
2018-08-01 17:10:04 -0600

    Merge

--- utility/src/main/java/ticket/com/utility/model/GameMap.java
+++ utility/src/main/java/ticket/com/utility/model/GameMap.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import ticket.com.tickettoridegames.utility.web.Result;
+import ticket.com.utility.web.Result;
 
 import ticket.com.utility.web.Result;
 
