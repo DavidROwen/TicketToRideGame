2df23bd5e52872a8244ee579e5aa0f9178a30c92
Ezra Clark <ezrajclark@gmail.com>
2018-08-08 14:32:54 -0600

    DatabaseManager functions

--- server/src/main/java/ticket/com/server/server/DB/IDbFactory.java
+++ server/src/main/java/ticket/com/server/server/DB/IDbFactory.java
@@ -1,4 +1,12 @@
 package ticket.com.server.server.DB;
 
 public interface IDbFactory {
+    //Integer commandCount;
+
+    void getInstance();
+    void startTransaction();
+    void finishTransaction(Boolean commit);
+    //IGameDAO getGameDAO();
+    //IUserDao getUserDAO();
+    //ICommandDAO getCommandDAO();
 }
