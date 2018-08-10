3a3bffeac010c005dbbdd2c1fedbab9a5c84face
Dillon Astin <astindillon@gmail.com>
2018-08-10 14:31:46 -0600

    set up a helper class

--- jsondb/src/main/java/ticket/com/jsondb/dao/JsonUserDao.java
+++ jsondb/src/main/java/ticket/com/jsondb/dao/JsonUserDao.java
@@ -1,15 +1,7 @@
 package ticket.com.jsondb.dao;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,36 +10,38 @@ import ticket.com.utility.model.User;
 
 public class JsonUserDao implements IUserDAO {
     private File file;
-    private Gson gson;
+    private JsonDao helper;
 
     public JsonUserDao(File file){
         this.file = file;
-        gson = new Gson();
+        helper = new JsonDao();
     }
 
     public Boolean addUser(User user){
-        List<User> users = getCurrentJson();
-        if(users == null){
-            users = new ArrayList<>();
-        }
-        users.add(user);
-        String json = gson.toJson(users);
-        try{
-            FileWriter fw = new FileWriter(file, false);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(json);
-            bw.flush();
-            bw.close();
-            return true;
+        List<User> users;
+        try {
+            users = (List<User>) helper.getCurrentJson(file.getCanonicalPath());
         }
         catch (IOException e){
             e.printStackTrace();
             return false;
         }
+        if(users == null){
+            users = new ArrayList<>();
+        }
+        users.add(user);
+        String json = helper.toJson(users);
+        return helper.writeToFile(file, json);
     }
 
     public User getUser(String username){
-        List<User> users = getCurrentJson();
+        List<User> users = null;
+        try {
+            users = (List<User>) helper.getCurrentJson(file.getCanonicalPath());
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
         if(users == null){
             return null;
         }
@@ -61,26 +55,7 @@ public class JsonUserDao implements IUserDAO {
         }
     }
 
-    public void clearUsers(){
-        try {
-            PrintWriter pw = new PrintWriter(file);
-            pw.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    private List<User> getCurrentJson(){
-        try {
-            byte[] encoded = Files.readAllBytes(Paths.get(file.getCanonicalPath()));
-            String currentJson = new String(encoded);
-            return gson.fromJson(currentJson, new TypeToken<List<User>>() {}.getType());
-        }
-        catch (IOException e){
-            e.printStackTrace();
-            return null;
-        }
+    public Boolean clearUsers(){
+        return helper.clearFile(file);
     }
 }
