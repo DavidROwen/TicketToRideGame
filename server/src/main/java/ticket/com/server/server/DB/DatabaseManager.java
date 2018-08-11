eaa3bffe6849ca70a3d3ec6933af61d4ec2b96ee
Dillon Astin <astindillon@gmail.com>
2018-08-11 13:09:32 -0600

    got rid of dupliate variable

--- server/src/main/java/ticket/com/server/server/DB/DatabaseManager.java
+++ server/src/main/java/ticket/com/server/server/DB/DatabaseManager.java
@@ -13,7 +13,6 @@ import ticket.com.utility.web.Command;
 public class DatabaseManager {
     IDbFactory dbFactory = null;
 
-    IDbFactory factory = null;
     Integer commandCount;
     Integer refreshCount;
     ServerModel sm = ServerModel.getInstance();
@@ -50,7 +49,7 @@ public class DatabaseManager {
     }
 
     public void assignFactory(IDbFactory factory){
-        this.factory = factory;
+        this.dbFactory = factory;
     }
 
     public void assignRefreshCount(Integer n){
@@ -64,13 +63,13 @@ public class DatabaseManager {
         }
 
         try {
-            factory.getCommandDAO().addCommand(command);
+            dbFactory.getCommandDAO().addCommand(command);
             commandCounts.put(gameID, commandCounts.get(gameID) + 1);
 
             if(commandCounts.get(gameID) == refreshCount){
                 commandCounts.put(gameID, 0);
-                factory.getCommandDAO().clearCommands();
-                factory.getGameDAO().updateGame(gameID, sm.getGameById(gameID)); //update game for n commands
+                dbFactory.getCommandDAO().clearCommands();
+                dbFactory.getGameDAO().updateGame(gameID, sm.getGameById(gameID)); //update game for n commands
             }
             return true;
         }
@@ -81,7 +80,7 @@ public class DatabaseManager {
     //Called by the ServerModel, serializes the user and sends it to IUserDAO
     public Boolean addUser(User user){
         try {
-            factory.getUserDAO().addUser(user);
+            dbFactory.getUserDAO().addUser(user);
             return true;
         }
         catch (Exception e){
@@ -98,14 +97,14 @@ public class DatabaseManager {
     }
 
     public void clearCommands(){
-        factory.clear();
+        dbFactory.clear();
 
     }
 
     //Called by the ServerModel, serializes the game and sends it to IGameDAO
     public Boolean addGame(Game game){
         try {
-            factory.getGameDAO().addGame(game);
+            dbFactory.getGameDAO().addGame(game);
             return true;
         }
         catch (Exception e){
@@ -115,30 +114,31 @@ public class DatabaseManager {
 
     //called by ServerCommunicator, tells ICommandDAO, IUserDAO, and IGameDAO to clear database
     public void clearDatabase(){
-        factory.getGameDAO().clearGames();
-        factory.getUserDAO().clearUsers();
-        factory.getCommandDAO().clearCommands();
+        dbFactory.getGameDAO().clearGames();
+        dbFactory.getUserDAO().clearUsers();
+        dbFactory.getCommandDAO().clearCommands();
     }
 
     //When the Server reboots it calls this to get the User from the factory
     public User getUser(String userID){
-        return factory.getUserDAO().getUser(userID);
+        return dbFactory.getUserDAO().getUser(userID);
     }
     //When the Server reboots it gives the most recent command and asks for
     // all commands after from the ICommandDAO
     public Command getCommmand(String commandID){
-        return factory.getCommandDAO().getCommand(commandID);
+        return null;
+        //return dbFactory.getCommandDAO().getCommand(commandID);
     }
     //When the Server reboots it calls this to get the Game from the IGameDAO
     public Game getGame(String gameID){
-        return factory.getGameDAO().getGame(gameID);
+        return dbFactory.getGameDAO().getGame(gameID);
     }
 
     // called by the ServerModel every n commands, it
     // clears the recent commands, re-serializes, and sends the game to the factory
     public Boolean updateGame(String gameID, Game game){
         try {
-            factory.getGameDAO().updateGame(gameID, game);
+            dbFactory.getGameDAO().updateGame(gameID, game);
             return true;
         }
         catch (Exception e){
